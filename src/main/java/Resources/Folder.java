5505745c4681c1afb902bbb82638ab3a74a2072a
Stuart Black <stuartblack@gmail.com>
2018-08-14 17:24:00 +0100

    Initial files and setup in place.

---
+++ src/main/java/Resources/Folder.java
@@ -0,0 +1,11 @@
+package Resources;
+
+import java.util.List;
+
+public class Folder {
+    private String name;
+    private String extension;
+    private int size;
+    private List<Folder> folders;
+
+}
