198be4d97527d0e329b2ca93877e04652e7ea553
Stuart Black <stuartblack@gmail.com>
2018-08-14 20:51:49 +0100

    CRUD + getAll functions tested ok

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -2,13 +2,40 @@ import db.DBHelper;
 import models.File;
 import models.Folder;
 
+import java.util.List;
+
 public class Runner {
 
     public static void main(String[] args) {
         Folder wallpapers = new Folder("Wallpapers");
         DBHelper.save(wallpapers);
-        File background1 = new File("background1", "png", 512, wallpapers);
+        Folder holiday = new Folder("Holiday");
+        DBHelper.save(holiday);
+        File background1 = new File("Kittens", "png", 512, wallpapers);
+        File background2 = new File("Sloths", "png", 256, wallpapers);
+        File background3 = new File("Fast Cars", "png", 1024, wallpapers);
+        File background4 = new File("Stars", "png", 2048, wallpapers);
+        File background5 = new File("3d Rendered", "png", 128, wallpapers);
+        File background6 = new File("DCIM-00001", "png", 64, holiday);
+        File background7 = new File("Spice Girls", "png", 32, wallpapers);
         DBHelper.save(background1);
+        DBHelper.save(background2);
+        DBHelper.save(background3);
+        DBHelper.save(background4);
+        DBHelper.save(background5);
+        DBHelper.save(background6);
+        DBHelper.save(background7);
+        background4.setFileName("Stars, stars, stars");
+        DBHelper.update(background4);
+        DBHelper.delete(background7);
+
+        List<File> files =  DBHelper.getAll(File.class);
+        System.out.println(files);
+        List<Folder> folders =  DBHelper.getAll(Folder.class);
+        System.out.println(folders);
+
+
+
 
     }
 }
--- src/main/java/db/DBHelper.java
+++ src/main/java/db/DBHelper.java
@@ -1,8 +1,12 @@
 package db;
 
+import org.hibernate.Criteria;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
+
+import java.util.List;
 
 public class DBHelper {
 
@@ -22,7 +26,62 @@ public class DBHelper {
             session.close();
         }
     }
+    public static void update(Object object){
+        session = HibernateUtil.getSessionFactory().openSession();
+        try {
+            transaction = session.beginTransaction();
+            session.update(object);
+            transaction.commit();
+        } catch(HibernateException ex) {
+            transaction.rollback();
+            ex.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
 
+    public static void delete(Object object){
+        session = HibernateUtil.getSessionFactory().openSession();
+        try {
+            transaction = session.beginTransaction();
+            session.delete(object);
+            transaction.commit();
+        } catch(HibernateException ex) {
+            transaction.rollback();
+            ex.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+    public static <T> T findById(Class classType, int id){
+        session = HibernateUtil.getSessionFactory().openSession();
+        T result = null;
+        try {
+            Criteria cr = session.createCriteria(classType);
+            cr.add(Restrictions.eq("id", id));
+            result = (T) cr.uniqueResult();
+        } catch(HibernateException ex) {
+            ex.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return result;
+    }
+
+    public static <T> List<T> getAll(Class classType) {
+        session = HibernateUtil.getSessionFactory().openSession();
+        List<T> results = null;
+        try {
+            Criteria cr = session.createCriteria(classType);
+            results = cr.list();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return results;
+    }
 
 
 
